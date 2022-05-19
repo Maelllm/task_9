519fcc3017e2536fafd632eb053962e9635945e6
Stanislav Liamets <104363322+Maelllm@users.noreply.github.com>
2022-05-20 00:15:10 +0300

    Update stas_task_1_hw_9.py

--- stas_task_1_hw_9.py
+++ stas_task_1_hw_9.py
@@ -1,39 +1,11 @@
-a = 1
-b = a
-id(a)
-id(b)
-a = 2
-a != b
-a = []
-b = a
-a.append('asdf')
-b = a
+def edit_number():
+    code: str = '+380'
+
+    def edit_code(number):
+        print(f'{code}{number}')
+
+    return edit_code
 
 
-def f():
-    x = 1  # Local
-
-
-def one():
-    x = ['one', 'two']
-
-    def inner():
-        print(x)
-        print(id(x))
-
-    return inner
-
-
-o = one()
-o()
-dir(o)
-o.__closure__
-o.__closure__[0]
-dir(o.__closure__[0])
-o.__closure__[0].cell_contents
-a = o.__closure__[0].cell_contents
-id(a)
-a.append('asdf')
-o()
-
-id(a)
+a = edit_number()
+print(a(4541531523))
